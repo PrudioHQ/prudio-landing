8316871682724a82f68d0753338ce8e6866976f2
Marios Antonoudiou <marios.ant@qboss.eu>
2014-12-05 19:07:59 +0000

    Fix mailchimp bug

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-  
-  $('.registration-form').submit(function (e) {
-    e.preventDefault();
-
-    $.ajax({
-        url: '//prud.us9.list-manage.com/subscribe/post?u=39ef96cac4b6f34b25ebb0ed0&amp;id=fd630d7a55',
-        type: 'GET',
-        data: $('#your-form').serialize(),
-        dataType: 'json',
-        contentType: "application/json; charset=utf-8",
-        success: function (data) {
-           if (data['result'] != "success") {
-                //ERROR
-                console.log(data['msg']);
-           } else {
-              $("input, button").attr("disabled", "disabled");
-              $("button").html('<span class="glyphicon glyphicon-ok success" aria-hidden="true"></span>');
-           }
-        }
-    });
-  });
-  
+  // Nothing here yet :)
 });
